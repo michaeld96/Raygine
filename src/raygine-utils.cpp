f62ad30698b56868943e46d745c811349f2695a1
Michael Dick <michaeldick000@gmail.com>
2024-09-14 13:31:19 -0400

    Took out a debug statement to display to the console what the currecnt directory was.

--- src/raygine-utils.cpp
+++ src/raygine-utils.cpp
@@ -3,7 +3,6 @@ namespace Raygine
 {
 rapidjson::Document RaygineUtils::LoadJsonFile(const std::string& file_path)
 {
-    std::cout << "Current working directory:" << std::filesystem::current_path() << std::endl;
     std::ifstream input_file(file_path);
     if (!input_file.is_open())
     {
